8152a5978319b06f6b46f5590af72f4877aa6476
ren <kikuren1st@gmail.com>
2024-03-22 15:02:59 +0900

    prisma

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -3,23 +3,15 @@ import { NextResponse } from "next/server";
 
 const prisma=new PrismaClient();//インスタンス化
 
-export async function main(){//ここのコードは非同期通信でprismaにつなる　エラーが起こったときにDB接続に失敗しましたと返す。
-    try{
-        await prisma.$connect();
-    }catch(err){
-        return Error("DB接続に失敗しました")
-    }
-}
-
 //ブログの全記事取得のAPI
 export const GET=async(req:Request,res:NextResponse)=>{
-    try{//とりあえずtry文の処理を実行してみるerrが発生したらcatch文に渡すerrが発生しなくてもfinally文に渡す
-        await main();
-        const posts=await prisma.post.findMany();//findManyは全記事取得の内容を表す　postはprismaのデータ型のを小文字にしたもの　ここでデータベースからデータ全取得
-        return NextResponse.json({message:"Success",posts},{status:200});//postに成功したときの内容とサーバーの情報200をかえす
+    try{
+        await prisma.$connect();
+        const posts=await prisma.post.findMany();
+        return NextResponse.json({message:"Success",posts},{status:200});
     }catch(err){
-        return NextResponse.json({message:"Error",err},{status:500});//エラーの内容とサーバーエラーの500番をかえす
+        return NextResponse.json({message:"Error",err},{status:500});
     }finally{
-        await prisma.$disconnect();//強制的にプリズマからの接続をきる
+        await prisma.$disconnect();
     }
 };
